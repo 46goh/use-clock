de97a2d28d894be839cb55676bda3158736bf06a
parasolr <palcum@gmail.com>
2019-10-01 01:05:09 +0900

    first commit of useClock

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-const test: string = 'test'
+import React, { useRef, useEffect, useState } from 'react'
+
+export default function useClock(duration: number) {
+  const [currentTime, setCurrentTime] = useState(Date.now())
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(Date.now())
+    })
+    return () => clearInterval(intervalId)
+  })
+
+  return currentTime
+}
